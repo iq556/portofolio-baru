4503e6138ac0cecd1dece65b79288deb52a28489
Mr X JOKER IQBAL <jokeriqbalmrx@gmail.com>
2025-12-30 08:26:14 +0700

    initial commit

---
+++ .js
@@ -0,0 +1,13 @@
+<script>
+const text = "Web Developer & Cybersecurity Enthusiast";
+let i = 0;
+
+function typing() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 80);
+  }
+}
+typing();
+</script>
